9a237681531cc03f10665f208abf43887cf417f3
hkpanghal <112403425+hkpanghal@users.noreply.github.com>
2024-09-07 14:32:05 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,84 @@
-import React, { useState } from 'react'
-import InputComponent from './components/InputComponent'
-import useCurrencyInfo from './hooks/useCurrencyInfo'
-
-
+import React, { useState } from "react";
+import InputComponent from "./components/InputComponent";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
+  let [currencyFrom, setCurrencyFrom] = useState("inr");
+  let [currencyTo, setCurrencyTo] = useState("usd");
 
-  let [currencyFrom,setCurrencyFrom] = useState("inr")
-  let [currencyTo,setCurrencyTo] = useState("usd")
+  let [inputAmountFrom, setinputAmountFrom] = useState(0);
+  let [inputAmountTo, setinputAmountTo] = useState(0);
 
-  let [inputAmountFrom,setinputAmountFrom] = useState(0)
-  let [inputAmountTo,setinputAmountTo] = useState(0)
+  let [covertedAmount, setConvertedAmount] = useState(0);
 
-  let [covertedAmount,setConvertedAmount] = useState(0)
+  let currencyInfoFrom = useCurrencyInfo(currencyFrom);
+  let currencyInfoTo = useCurrencyInfo(currencyTo);
 
-  let currencyInfoFrom = useCurrencyInfo(currencyFrom)
-  let currencyInfoTo= useCurrencyInfo(currencyTo)
+  let currencyOptionsFrom = Object.keys(currencyInfoFrom);
+  let currencyOptionsTo = Object.keys(currencyInfoTo);
 
-  let currencyOptionsFrom = Object.keys(currencyInfoFrom)
-  let currencyOptionsTo = Object.keys(currencyInfoTo)
-
-  let swap = ()=>{
-   
+  let swap = () => {
     let temp1 = inputAmountFrom;
     inputAmountFrom = inputAmountTo;
     inputAmountTo = temp1;
-    setinputAmountFrom(inputAmountFrom)
-    setinputAmountTo(inputAmountTo)
+    setinputAmountFrom(inputAmountFrom);
+    setinputAmountTo(inputAmountTo);
 
     let temp2 = currencyFrom;
     currencyFrom = currencyTo;
-    currencyTo = temp2
-    setCurrencyFrom(currencyFrom)
-    setCurrencyTo(currencyTo)
+    currencyTo = temp2;
+    setCurrencyFrom(currencyFrom);
+    setCurrencyTo(currencyTo);
     // setConvertedAmount(amount)
+  };
 
-  }
+  let convert = () => {
+    let amountConverted = inputAmountFrom * currencyInfoFrom[currencyTo];
+    setinputAmountTo(amountConverted);
 
-  let convert = ()=>{
-
-    let amountConverted = inputAmountFrom * currencyInfoFrom[currencyTo]
-    setinputAmountTo(amountConverted)
-
-    
-    setConvertedAmount(amountConverted)
-
-    
-    
-  }
+    setConvertedAmount(amountConverted);
+  };
 
   return (
-  <div className='w-full h-screen  bg-blue-400 text-white flex items-center justify-center px-8  bg-[url("https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3RvY2slMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D")] bg-no-repeat bg-cover'>
+    <div className='w-full h-screen  bg-blue-400 text-white flex items-center justify-center px-8  bg-[url("https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3RvY2slMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D")] bg-no-repeat bg-cover'>
+      <div
+        className="w-fit h-fit rounded-md p-8 flex items-center justify-center flex-col gap-2 shadow-sm shadow-black"
+        style={{ backgroundColor: "rgba(255, 255, 255, 0.300)" }}
+      >
+        <InputComponent
+          inputDisable={false}
+          inputLabel="From"
+          inputAmount={inputAmountFrom}
+          setinputAmount={setinputAmountFrom}
+          currency={currencyFrom}
+          setCurrency={setCurrencyFrom}
+          currencyOptions={currencyOptionsFrom}
+        />
 
-    <div className='w-fit h-fit rounded-md p-8 flex items-center justify-center flex-col gap-2 shadow-sm shadow-black' style={{backgroundColor:"rgba(255, 255, 255, 0.300)"}}>
+        <button
+          className="bg-blue-800 px-10 py-4 rounded-md shadow-inner shadow-white  hover:bg-blue-600"
+          onClick={() => swap()}
+        >
+          Swap
+        </button>
 
-      <InputComponent inputDisable={false} inputLabel="From" inputAmount={inputAmountFrom} setinputAmount={setinputAmountFrom} currency={currencyFrom} setCurrency={setCurrencyFrom} currencyOptions={currencyOptionsFrom} />
+        <InputComponent
+          inputLabel="To"
+          inputAmount={inputAmountTo}
+          currency={currencyTo}
+          setCurrency={setCurrencyTo}
+          currencyOptions={currencyOptionsTo}
+        />
 
-      <button className='bg-blue-800 px-10 py-4 rounded-md shadow-inner shadow-white  hover:bg-blue-600' onClick={()=>swap()}>Swap</button>
-
-      <InputComponent  inputLabel="To" inputAmount={inputAmountTo}  currency={currencyTo} setCurrency={setCurrencyTo} currencyOptions={currencyOptionsTo} />
-       
-      <button className='bg-blue-800 w-full px-10 py-4 rounded-md shadow-inner shadow-white hover:bg-blue-600' onClick={()=>convert()}>convert</button>
-
+        <button
+          className="bg-blue-800 w-full px-10 py-4 rounded-md shadow-inner shadow-white hover:bg-blue-600"
+          onClick={() => convert()}
+        >
+          convert
+        </button>
+      </div>
     </div>
-    
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -1,41 +1,49 @@
-import React from 'react'
+import React from "react";
 
-function InputComponent({inputDisable=true, inputLabel, inputAmount , setinputAmount , currency = "" , setCurrency ,currencyOptions=["inr","usd"]},convert) {
-
+function InputComponent(
+  {
+    inputDisable = true,
+    inputLabel,
+    inputAmount,
+    setinputAmount,
+    currency = "",
+    setCurrency,
+    currencyOptions = ["inr", "usd"],
+  },
+  convert
+) {
   return (
-    <div className='w-full h-1/2 flex bg-zinc-800 rounded-md text-orange-500  justify-between px-8 py-4 shadow-inner shadow-white '>
-      <div className='left h-full  rounded-md flex  justify-center flex-col gap-2'>
+    <div className="w-full h-1/2 flex bg-zinc-800 rounded-md text-orange-500  justify-between px-8 py-4 shadow-inner shadow-white ">
+      <div className="left h-full  rounded-md flex  justify-center flex-col gap-2">
         <label htmlFor="inputLabel">{inputLabel}</label>
 
-        <input type="number" className='w-[80%] rounded-md px-4 py-2'  
-        value={inputAmount} 
-        disabled={inputDisable}
-        onChange={(e)=> {
-         setinputAmount && setinputAmount(Number(e.target.value) )
-       
-        }}
-        
+        <input
+          type="number"
+          className="w-[80%] rounded-md px-4 py-2"
+          value={inputAmount}
+          disabled={inputDisable}
+          onChange={(e) => {
+            setinputAmount && setinputAmount(Number(e.target.value));
+          }}
         />
-
       </div>
 
-      <div className='right h-full  flex justify-center flex-col gap-2 '>
+      <div className="right h-full  flex justify-center flex-col gap-2 ">
         <p>Currency</p>
-        <select name="" id="" className='rounded-md  py-1.5' 
-         value={currency} 
-         onChange={(e)=> setCurrency && setCurrency(e.target.value)}>
-
-         {
-            currencyOptions.map
-            (
-              (currency)=> (<option value={currency}>{currency}</option>)
-            )
-         }
-
+        <select
+          name=""
+          id=""
+          className="rounded-md  py-1.5"
+          value={currency}
+          onChange={(e) => setCurrency && setCurrency(e.target.value)}
+        >
+          {currencyOptions.map((currency) => (
+            <option value={currency}>{currency}</option>
+          ))}
         </select>
       </div>
     </div>
-  )
+  );
 }
 
-export default InputComponent
+export default InputComponent;
